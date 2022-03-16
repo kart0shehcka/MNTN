7afc32f7d1a1c4519edd2279558f428c50f001cc
kart0shechka <kart0shechka.70@gmail.com>
2022-03-16 15:59:05 +0300

    фиксация: "main"

---
+++ js/new.js
@@ -0,0 +1,6 @@
+const navBtn = document.querySelector('.nav-button');
+const mobileNav = document.querySelector('.mobile-nav');
+
+navBtn.addEventListener('click', function () {
+	mobileNav.classList.toggle('mobile-nav-active');
+});
