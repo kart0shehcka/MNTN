f1e34211d98f8575a8400789c53320665f203cf0
kart0shechka <kart0shechka.70@gmail.com>
2022-03-19 15:28:13 +0300

    adapted footer and burger-menu

--- js/new.js
+++ js/new.js
@@ -1,6 +1,33 @@
 const navBtn = document.querySelector('.nav-button');
 const mobileNav = document.querySelector('.mobile-nav');
+const bodyScroll = document.body;
 
-navBtn.addEventListener('click', function () {
+
+
+
+
+
+
+navBtn.addEventListener('click', function (event) {
+	event.stopPropagation('click');
 	mobileNav.classList.toggle('mobile-nav-active');
+	bodyScroll.classList.toggle('no-scroll');
+});
+
+
+
+window.addEventListener('click', function () {
+	console.log("Clik on window");
+
+
+	if (bodyScroll.classList.contains('no-scroll')) {
+		mobileNav.classList.toggle('mobile-nav-active');
+		bodyScroll.classList.toggle('no-scroll');
+	}
+});
+
+
+
+mobileNav.addEventListener('click', function (event) {
+	event.stopPropagation('click');
 });
